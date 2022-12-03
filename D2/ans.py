b9c21b182bb472d7666ed17304dc363554de84a0
derek_lee <20820343@student.curtin.edu.au>
2022-12-03 19:32:09 +0400

    added readme for D2

--- D2/ans.py
+++ D2/ans.py
@@ -1,37 +1,40 @@
-# P1
+# Advent Of Code
+# Author: Derek Lee
+# Language: Python
+# Day: 2
 
-# class RPS:
-#     def __init__(self):
-#         self.totalPoints = 0
-#         self.pts = {"X" : 1, "Y" : 2, "Z" : 3}
-#
-#     def gameplay(self, p1, p2):
-#         if p2 == "X":
-#             if p1 == "B":
-#                 self.totalPoints += self.pts[p2]
-#             elif p1 == "C":
-#                 self.totalPoints += self.pts[p2] + 6
-#             else:
-#                 self.totalPoints += self.pts[p2] + 3
-#         elif p2 == "Y":
-#             if p1 == "A":
-#                 self.totalPoints += self.pts[p2] + 6
-#             elif p1 == "C":
-#                 self.totalPoints += self.pts[p2]
-#             else:
-#                 self.totalPoints += self.pts[p2] + 3
-#         elif p2 == "Z":
-#             if p1 == "A":
-#                 self.totalPoints += self.pts[p2]
-#             elif p1 == "B":
-#                 self.totalPoints += self.pts[p2] + 6
-#             else:
-#                 self.totalPoints += self.pts[p2] + 3
+# Part 1
+class RPS1:
+    def __init__(self):
+        self.totalPoints = 0
+        self.pts = {"X" : 1, "Y" : 2, "Z" : 3}
 
-# P2
+    def gameplay(self, p1, p2):
+        if p2 == "X":
+            if p1 == "B":
+                self.totalPoints += self.pts[p2]
+            elif p1 == "C":
+                self.totalPoints += self.pts[p2] + 6
+            else:
+                self.totalPoints += self.pts[p2] + 3
+        elif p2 == "Y":
+            if p1 == "A":
+                self.totalPoints += self.pts[p2] + 6
+            elif p1 == "C":
+                self.totalPoints += self.pts[p2]
+            else:
+                self.totalPoints += self.pts[p2] + 3
+        elif p2 == "Z":
+            if p1 == "A":
+                self.totalPoints += self.pts[p2]
+            elif p1 == "B":
+                self.totalPoints += self.pts[p2] + 6
+            else:
+                self.totalPoints += self.pts[p2] + 3
 
 
-class RPS:
+# Part 2
+class RPS2:
     def __init__(self):
         self.totalPoints = 0
         self.pts = {"X": 0, "Y": 3, "Z": 6}
@@ -57,10 +60,13 @@ class RPS:
 
         self.totalPoints += self.pts[p2]
 
-gameObj = RPS()
+gameObj = RPS1()
+gameObj2 = RPS2()
 with open("test.txt") as f:
     for line in f:
         inp = line.split()
         gameObj.gameplay(inp[0], inp[1])
+        gameObj2.gameplay(inp[0], inp[1])
 
 print(gameObj.totalPoints)
+print(gameObj2.totalPoints)
