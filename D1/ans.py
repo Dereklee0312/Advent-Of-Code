4655f8e69caee6a48ea3aafec6c77d2410f47447
derek_lee <20820343@student.curtin.edu.au>
2022-12-03 19:27:17 +0400

    misc

--- D1/ans.py
+++ D1/ans.py
@@ -3,17 +3,18 @@
 # Language: Python
 # Day: 1
 
-# Part 1
 calList = []
 totalCal = 0
 with open("test.txt") as f:
     for line in f:
+        # Reset the totalCal to 0 as new Elf is entering data
         if line.startswith("\n"):
             calList.append(totalCal)
             totalCal = 0
         else:
             totalCal += int(line)
 
+# Part 1
 print(f'Elf with most calories has: {max(calList)}')
 
 # Part 2
