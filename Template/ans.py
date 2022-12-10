b2d798b2b8cabab2b43f0024a40059bff0241ab5
derek_lee <20820343@student.curtin.edu.au>
2022-12-10 16:10:29 +0400

    Template: added helper file and removed ans2.py

--- Template/ans.py
+++ Template/ans.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-import sys
 
-if len(sys.argv) == 1:
-    filename = "demo.txt"
-else:
-    filename = "input.txt"
-
-lines = []
-with open(filename) as f:
-    for line in f:
-        lines.append(line.strip())
+from helper import parseFile
