c927fed43a1483655b17d2785b4faa829e3061ce
derek_lee <20820343@student.curtin.edu.au>
2022-12-12 16:45:10 +0400

    D12: Completed P1

--- D12/ans.py
+++ D12/ans.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
 
-from helper import parseFile
+from helper import *
 
-lines = parseFile()
+sRow, sCol, maxRow, maxCol, lines = parseFile()
+
+dict = createDict()
+
+dict["S"] = 1
+dict["E"] = 26
+grid = []
+
+for i, line in enumerate(lines):
+    lst = []
+    for j, a in enumerate(line):
+        point = Point(a, dict[a], i, j)
+        lst.append(point)
+    grid.append(lst)
+
+print(move(grid, sRow, sCol, maxRow, maxCol))
--- D12/helper.py
+++ D12/helper.py
@@ -1,14 +1,87 @@
 import sys
 
+
 def parseFile():
     if len(sys.argv) == 1:
         filename = "demo.txt"
+        maxRow = 5
+        maxCol = 8
+        sRow, sCol = 0, 0
     else:
         filename = "input.txt"
+        maxRow = 41
+        maxCol = 162
+        sRow, sCol = 20, 0
 
     lines = []
     with open(filename) as f:
         for line in f:
             lines.append(line.strip())
 
-    return lines
+    return sRow, sCol, maxRow, maxCol, lines
+
+
+def createDict():
+    alpha = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+    ]
+    dict = {}
+    for i in range(26):
+        dict[alpha[i]] = i + 1
+
+    return dict
+
+
+class Point:
+    def __init__(self, char, value, row, col):
+        self.char = char
+        self.visited = False
+        self.value = value
+        self.row = row
+        self.col = col
+
+
+def move(grid, curRow, curCol, maxRow, maxCol):
+    movements = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+    curPos = grid[curRow][curCol]
+
+    queue = []
+    queue.append((curPos, 0))
+    curPos.visited = True
+
+    while queue:
+        start, count = queue.pop(0)
+
+        for i, j in movements:
+            if 0 <= start.row + i < maxRow and 0 <= start.col + j < maxCol:
+                next = grid[start.row + i][start.col + j]
+                if start.char == "E":
+                    return count
+                if next.value - start.value <= 1 and next.visited == False:
+                    next.visited = True
+                    queue.append((next, count + 1))
