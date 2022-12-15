72c53c60af661546bec57054a1d8e1853574fe3f
derek_lee <20820343@student.curtin.edu.au>
2022-12-16 00:50:46 +0400

    D14: Reformatted code with black

--- D14/ans2.py
+++ D14/ans2.py
@@ -15,7 +15,9 @@ rows, cols = [y for x, y in coords], [x for x, y in coords]
 
 MRow = max(rows) + 2
 mCol = min(cols)
-MCol = max(cols) + MRow * 2 # Adding MRow * 2 since base of the path will not go wider than a pyramid shape.
+MCol = (
+    max(cols) + MRow * 2
+)  # Adding MRow * 2 since base of the path will not go wider than a pyramid shape.
 
 grid = [["." for _ in range(MCol - mCol + 1)] for _ in range(MRow + 1)]
 
@@ -35,13 +37,17 @@ for cmd in lines:
         coords.append((int(xy.split(",")[1]), int(xy.split(",")[0])))
     for i, (row, col) in enumerate(coords):
         if i + 1 < len(coords):
-            for a in range( x := abs(row - coords[i+1][0]) + 1 if row != coords[i+1][0] else 0):
-                if row > coords[i+1][0]:
+            for a in range(
+                (x := abs(row - coords[i + 1][0]) + 1 if row != coords[i + 1][0] else 0)
+            ):
+                if row > coords[i + 1][0]:
                     a = -a
                 grid[row + a][col - mCol] = "#"
 
-            for b in range( x := abs(col - coords[i+1][1]) + 1 if col != coords[i+1][1] else 0):
-                if col > coords[i+1][1]:
+            for b in range(
+                (x := abs(col - coords[i + 1][1]) + 1 if col != coords[i + 1][1] else 0)
+            ):
+                if col > coords[i + 1][1]:
                     b = -b
                 grid[row][col + b - mCol] = "#"
 
@@ -55,12 +61,12 @@ count = 0
 # Continue looping until starting position has been replaced by "o"
 while start:
     count += 1
-    cont = moveSand2(0, 500 - mCol, grid, MRow,  500-mCol + 162)
-    start = False if grid[0][500-mCol] == "o" else True
+    cont = moveSand2(0, 500 - mCol, grid, MRow, 500 - mCol + 162)
+    start = False if grid[0][500 - mCol] == "o" else True
 
-for i in range(deltaRow):
-    for j in range(deltaCol):
-        print(grid[i][j], end="")
-    print()
+# for i in range(deltaRow):
+#     for j in range(deltaCol):
+#         print(grid[i][j], end="")
+#     print()
 
 print(count)
