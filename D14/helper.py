00822b3731c34714397000d4cfa2c65193205a80
derek_lee <20820343@student.curtin.edu.au>
2022-12-15 17:39:28 +0400

    D14: Completed P2

--- D14/helper.py
+++ D14/helper.py
@@ -51,3 +51,43 @@ def moveSand(sRow, sCol, grid, MRow):
                         break
         else:
             return -1
+
+def moveSand2(sRow, sCol, grid, MRow, MCol):
+    cont = True
+    curRow = sRow
+    curCol = sCol
+    down = (1, 0)
+    SW = (1, -1)
+    SE = (1, 1)
+    restPts = ["o", "#"]
+    while cont:
+        nextRow = curRow + down[0]
+        nextCol = curCol + down[1]
+        if nextRow <= MRow and nextCol <= MCol:
+            try:
+                if grid[nextRow][nextCol] not in restPts:
+                    curRow = nextRow
+                    curCol = nextCol
+                    continue
+                else:
+                    nextRow = curRow + SW[0]
+                    nextCol = curCol + SW[1]
+
+                    if grid[nextRow][nextCol] not in restPts:
+                        curRow = nextRow
+                        curCol = nextCol
+                        continue
+                    else:
+                        nextRow = curRow + SE[0]
+                        nextCol = curCol + SE[1]
+                        if grid[nextRow][nextCol] not in restPts:
+                            curRow = nextRow
+                            curCol = nextCol
+                            continue
+                        else:
+                            grid[curRow][curCol] = "o"
+                            break
+            except:
+                break
+        else:
+            return -1
