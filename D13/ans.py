132dc6d285872313e05335e3ddf71fb1d252cb0d
derek_lee <20820343@student.curtin.edu.au>
2022-12-19 13:57:45 +0400

    D13: Completed P1

--- D13/ans.py
+++ D13/ans.py
@@ -14,25 +14,22 @@ for i, line in enumerate(lines):
     if step == 0:
         left = eval(line)
         step += 1
-        # print("left: ", left)
     elif step == 1:
         right = eval(line)
-        # print("right: ", right)
         step += 1
     elif step == 2:
         step = 0
         num += 1
         print("STARTING COMPARISON", num)
-        if compareLines(left, right) == 1:
+        if compareLst(left, right) == 1:
             print("APPROVED")
-            # print(num)
             sum += num
         else:
             print("DENIED")
 else:
     num += 1
     print("STARTING COMPARISON", num)
-    if compareLines(left, right) == 1:
+    if compareLst(left, right) == 1:
         print("APPROVED")
         print(num)
         sum += num
--- D13/helper.py
+++ D13/helper.py
@@ -15,65 +15,33 @@ def parseFile():
     return lines
 
 
-def compareLines(lst1, lst2):
-    while lst1 and lst2:
-        left = lst1.pop(0)
-        right = lst2.pop(0)
-        print(left)
-        print(right, "\n")
-
-        if type(left) == type(right) == int:
-            if left > right:
-                return -1
-            elif left == right:
-                continue
-            else:
-                return 1
-        elif type(left) == list and type(right) == int:
-            right = [right]
-            comparison = compareLst(left, right)
-            if comparison == 0:
-                continue
-            else:
-                return comparison
-
-        elif type(left) == int and type(right) == list:
-            left = [left]
-            comparison = compareLst(left, right)
-            if comparison == 0:
-                continue
-            else:
-                return comparison
-
-        else:
-            comparison = compareLst(left, right)
-            if comparison == 0:
-                continue
-            else:
-                return comparison
-
-    if len(lst1) < len(lst2):
-        return 1
-    else:
-        return -1
-
 def compareLst(lst1, lst2):
-    for i in range(len(lst1)):
-        left = lst1[i]
-        try:
-            right = lst2[i]
-            if left == right:
-                continue
-            elif left < right:
-                return 1
-            else:
-                return -1
-        except:
+    if type(lst1) == list and type(lst2) == int:
+        lst2 = [lst2]
+    if type(lst1) == int and type(lst2) == list:
+        lst1 = [lst1]
+    if type(lst1) == int and type(lst2) == int:
+        if lst1 < lst2:
+            return 1
+        elif lst1 > lst2:
             return -1
+        return 0
+    if type(lst1) == list and type(lst2) == list:
+        i = 0
 
-    if len(lst1) < len(lst2):
-        return 1
-    return 0
+        while i < len(lst1) and i < len(lst2):
+            cont = compareLst(lst1[i], lst2[i])
 
-# lst = eval("[1,[2,[3,[4,[5,6,0]]]],8,9]")
-# print(type(lst[0]))
+            if cont == 1:
+                return 1
+            elif cont == -1:
+                return -1
+
+            i += 1
+
+        if i == len(lst1):
+            if len(lst1) == len(lst2):
+                return 0
+            return 1
+
+        return -1
