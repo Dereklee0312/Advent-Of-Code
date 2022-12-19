d683d47d645ba246e4fba524c1098f207531d437
derek_lee <20820343@student.curtin.edu.au>
2022-12-19 14:16:52 +0400

    D13: Completed P2

--- D13/ans.py
+++ D13/ans.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from helper import *
+from functools import cmp_to_key
 
 lines = parseFile()
 left = []
@@ -10,30 +11,38 @@ sum = 0
 step = 0
 num = 0
 
+lst = []
+
 for i, line in enumerate(lines):
     if step == 0:
         left = eval(line)
+        lst.append(left)
         step += 1
     elif step == 1:
         right = eval(line)
+        lst.append(right)
         step += 1
     elif step == 2:
         step = 0
         num += 1
-        print("STARTING COMPARISON", num)
         if compareLst(left, right) == 1:
-            print("APPROVED")
             sum += num
-        else:
-            print("DENIED")
 else:
     num += 1
-    print("STARTING COMPARISON", num)
     if compareLst(left, right) == 1:
-        print("APPROVED")
-        print(num)
         sum += num
-    else:
-        print("DENIED")
 
-print(sum)
+lst.append([[2]])
+lst.append([[6]])
+
+lst = sorted(lst, key = cmp_to_key(compareLst), reverse = True)
+
+print("P1: ", sum)
+
+for i, L in enumerate(lst):
+    if L == [[2]]:
+        ans1 = i + 1
+    elif L == [[6]]:
+        ans2 = i + 1
+
+print("P2: ",ans1 * ans2)
