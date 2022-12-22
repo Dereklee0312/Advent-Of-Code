50ed6ab91e2f3aeffe6060c1206db2dcfc851581
derek_lee <20820343@student.curtin.edu.au>
2022-12-22 11:11:56 +0400

    D21: Completed P1

---
+++ D21/ans.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+from helper import *
+
+lines = parseFile()
+
+monkLst = {}
+mathLst = {}
+removeLst = []
+
+for line in lines:
+    spl = line.split()
+    if len(spl) == 2:
+        monkLst[spl[0].replace(":", "")] = int(spl[1])
+    else:
+        mathLst[spl[0].replace(":", "")] = (spl[1], spl[2], spl[3])
+
+
+while mathLst:
+    removeLst.clear()
+    for (key, val) in mathLst.items():
+        monk1 = val[0]
+        monk2 = val[2]
+
+        if monk1 in monkLst.keys() and monk2 in monkLst.keys():
+            result = compute(monkLst[monk1], monkLst[monk2], val[1])
+            removeLst.append((key, result))
+
+    for key, result in removeLst:
+        mathLst.pop(key)
+        monkLst[key] = result
+
+
+
+for (key, val) in monkLst.items():
+    print(key, val)
---
+++ D21/helper.py
@@ -0,0 +1,32 @@
+import sys
+
+def parseFile():
+    if len(sys.argv) == 1:
+        filename = "demo.txt"
+    else:
+        filename = "input.txt"
+
+    lines = []
+    with open(filename) as f:
+        for line in f:
+            lines.append(line.strip())
+
+    return lines
+
+"""
+Have to ensure all mathematical monkeys have already given an answer
+Can loop through each monkeys, those that have given their number are removed from list
+"""
+
+def compute(monk1: int, monk2: int, op: str):
+    if op == "+":
+        return monk1 + monk2
+
+    elif op == "-":
+        return monk1 - monk2
+
+    elif op == "*":
+        return monk1 * monk2
+
+    elif op == "/":
+        return monk1 / monk2
